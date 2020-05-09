9974bf2b73db823ad5517ae3ecf0e73d5d170ee1
Liang-Hsuan Ma <leonma333@hotmail.com>
2020-05-08 21:06:17 -0400

    fix lint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+/* tslint:disable:no-string-literal */
 export const environment = {
   production: false,
   firebase: {
