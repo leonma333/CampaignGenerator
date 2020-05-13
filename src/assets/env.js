a63d30d36e621cc3da4e4cccfe2dde7d30b69627
Liang-Hsuan Ma <leonma333@hotmail.com>
2020-05-13 14:19:24 -0400

    Patch

--- src/assets/env.js
+++ src/assets/env.js
@@ -2,12 +2,12 @@
   window.env = window.env || {};
 
   // Environment variables
-  window['env']['firebase_apiKey'] = 'AIzaSyAY9FcNhbMXnS7ak_9aFJVd3uNSc4UIZB4';
-  window['env']['firebase_authDomain'] = 'campaign-generator-333.firebaseapp.com';
-  window['env']['firebase_databaseURL'] = 'https://campaign-generator-333.firebaseio.com';
-  window['env']['firebase_projectId'] = 'campaign-generator-333';
-  window['env']['firebase_storageBucket'] = 'campaign-generator-333.appspot.com';
-  window['env']['firebase_messagingSenderId'] = '305863371753';
-  window['env']['firebase_appId'] = '1:305863371753:web:f7b7fdb5a4b53f47b67978';
-  window['env']['firebase_measurementId'] = '';
+  window['env']['firebase_apiKey'] = window['env']['firebase_apiKey'];
+  window['env']['firebase_authDomain'] = window['env']['firebase_authDomain'];
+  window['env']['firebase_databaseURL'] = window['env']['firebase_databaseURL'];
+  window['env']['firebase_projectId'] = window['env']['firebase_projectId'];
+  window['env']['firebase_storageBucket'] = window['env']['firebase_storageBucket'];
+  window['env']['firebase_messagingSenderId'] = window['env']['firebase_messagingSenderId'];
+  window['env']['firebase_appId'] = window['env']['firebase_appId'];
+  window['env']['firebase_measurementId'] = window['env']['firebase_measurementId'];
 })(this);
