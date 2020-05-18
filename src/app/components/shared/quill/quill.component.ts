4377346037991d301f5e5ca4172729e2578fbd8e
Liang-Hsuan Ma <leonma333@hotmail.com>
2020-05-18 10:44:17 -0400

    Preserve image align

--- src/app/components/shared/quill/quill.component.ts
+++ src/app/components/shared/quill/quill.component.ts
@@ -7,6 +7,36 @@ import 'quill-emoji/dist/quill-emoji.js';
 import ImageResize from 'quill-image-resize-module';
 Quill.register('modules/imageResize', ImageResize);
 
+const BaseImageFormat = Quill.import('formats/image');
+const ImageFormatAttributesList = [
+    'alt',
+    'height',
+    'width',
+    'style'
+];
+class ImageFormat extends BaseImageFormat {
+  static formats(domNode) {
+    return ImageFormatAttributesList.reduce(function(formats, attribute) {
+      if (domNode.hasAttribute(attribute)) {
+        formats[attribute] = domNode.getAttribute(attribute);
+      }
+      return formats;
+    }, {});
+  }
+  format(name, value) {
+    if (ImageFormatAttributesList.indexOf(name) > -1) {
+      if (value) {
+        this.domNode.setAttribute(name, value);
+      } else {
+        this.domNode.removeAttribute(name);
+      }
+    } else {
+      super.format(name, value);
+    }
+  }
+}
+Quill.register(ImageFormat, true);
+
 @Component({
   selector: 'app-quill',
   templateUrl: './quill.component.html',
