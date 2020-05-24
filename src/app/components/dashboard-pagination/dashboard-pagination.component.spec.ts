51c71934f21930a6417994c4848c22645e914f96
Liang-Hsuan Ma <leonma333@hotmail.com>
2020-05-24 10:46:43 -0400

    Add pagination test

--- src/app/components/dashboard-pagination/dashboard-pagination.component.spec.ts
+++ src/app/components/dashboard-pagination/dashboard-pagination.component.spec.ts
@@ -1,3 +1,5 @@
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { of } from 'rxjs';
@@ -38,12 +40,110 @@ describe('Component: DashboardPaginationComponent', () => {
 
     campaigns.forEach(campaign => campaign.doc = campaign.id);
     mockCampaignService = TestBed.inject(CampaignService);
-    mockCampaignService.getAll.and.returnValue(of(campaigns));
+    mockCampaignService.getAll.and.callFake((sort, options) => {
+        if (options && options.startAfter) {
+          if (options.startAfter === '7') {
+            return of([]);
+          }
+          if (options.startAfter === '6') {
+            return of([campaigns[6]]);
+          }
+          expect(options.startAfter).toEqual('3');
+          return of(campaigns.slice(3, 6));
+        } else if (options && options.startAt && options.endBefore) {
+          if (options.startAt === '1') {
+            expect(options.endBefore).toEqual('4');
+            return of(campaigns.slice(0, 3));
+          }
+          if (options.startAt === '4') {
+            expect(options.endBefore).toEqual('7');
+            return of(campaigns.slice(3, 6));
+          }
+          throw new Error('should not be here');
+        }
+        return of(campaigns.slice(0, 3));
+      });
+
+    spyOn(component.loading, 'emit');
+    spyOn(component.campaigns, 'emit');
 
     fixture.detectChanges();
   });
 
+  function nextPage() {
+    const nextPageEl: DebugElement = fixture.debugElement.query(By.css('.next-page'));
+    nextPageEl.nativeElement.click();
+  }
+
+  function prevPage() {
+    const prevPageEl: DebugElement = fixture.debugElement.query(By.css('.prev-page'));
+    prevPageEl.nativeElement.click();
+  }
+
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.page).toBe(1);
+    expect(component.disabledPrev).toBeTrue();
+    expect(component.disabledNext).toBeFalse();
+
+    let emitSpy = component.loading.emit as any;
+    expect(emitSpy).toHaveBeenCalledTimes(2);
+    expect(emitSpy.calls.allArgs()[0][0]).toBeTrue();
+
+    emitSpy = component.campaigns.emit as any;
+    expect(emitSpy).toHaveBeenCalledTimes(1);
+    expect(emitSpy.calls.allArgs()[0][0]).toEqual(campaigns.slice(0, 3));
+  });
+
+  it('should emit campaigns when change page', () => {
+    nextPage();
+    fixture.detectChanges();
+
+    expect(component.page).toBe(2);
+    expect(component.disabledPrev).toBeFalse();
+    expect(component.disabledNext).toBeFalse();
+
+    const emitSpy = component.campaigns.emit as any;
+    expect(emitSpy).toHaveBeenCalledTimes(2);
+    expect(emitSpy.calls.mostRecent().args[0]).toEqual(campaigns.slice(3, 6));
+
+    nextPage();
+    fixture.detectChanges();
+
+    expect(component.page).toBe(3);
+    expect(component.disabledPrev).toBeFalse();
+    expect(component.disabledNext).toBeFalse();
+
+    expect(emitSpy).toHaveBeenCalledTimes(3);
+    expect(emitSpy.calls.mostRecent().args[0]).toEqual([campaigns[6]]);
+
+    nextPage();
+    fixture.detectChanges();
+
+    expect(component.page).toBe(3);
+    expect(component.disabledPrev).toBeFalse();
+    expect(component.disabledNext).toBeTrue();
+
+    expect(emitSpy).toHaveBeenCalledTimes(3);
+
+    prevPage();
+    fixture.detectChanges();
+
+    expect(component.page).toBe(2);
+    expect(component.disabledPrev).toBeFalse();
+    expect(component.disabledNext).toBeFalse();
+
+    expect(emitSpy).toHaveBeenCalledTimes(4);
+    expect(emitSpy.calls.mostRecent().args[0]).toEqual(campaigns.slice(3, 6));
+
+    prevPage();
+    fixture.detectChanges();
+
+    expect(component.page).toBe(1);
+    expect(component.disabledPrev).toBeTrue();
+    expect(component.disabledNext).toBeFalse();
+
+    expect(emitSpy).toHaveBeenCalledTimes(5);
+    expect(emitSpy.calls.mostRecent().args[0]).toEqual(campaigns.slice(0, 3));
   });
 });
